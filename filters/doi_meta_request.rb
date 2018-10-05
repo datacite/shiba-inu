daa98b2b44d3b2b63d24584950fd9563bd9136c9
Kristian Garza <kj.garza@gmail.com>
2018-10-05 16:02:46 +0200

    error catching

--- filters/doi_meta_request.rb
+++ filters/doi_meta_request.rb
@@ -1,4 +1,5 @@
 require "faraday"
+require 'json'
 
 API_URL = "https://api.datacite.org"
 
@@ -15,23 +16,31 @@ end
 def filter(event)
   total = event.get("[dois][buckets]")
   dois = total.map do |dataset| 
+
+    unique_regular = dataset["access_method"]["buckets"].find {|access_method| access_method['key'] == 'regular' }
+    unique_machine = dataset["access_method"]["buckets"].find {|access_method| access_method['key'] == 'machine' }
+    total_regular  = dataset["total"]["buckets"].find {|access_method| access_method['key'] == 'regular' }
+    total_machine  = dataset["total"]["buckets"].find {|access_method| access_method['key'] == 'machine' }
+
     puts dataset["key"]
     { 
       doi: dataset["key"], 
-      unique_counts_regular: dataset["access_method"]["buckets"].find {|access_method| access_method['key'] == 'regular' }["unqiue"]["value"],
-      unique_counts_machine: dataset["access_method"]["buckets"].find {|access_method| access_method['key'] == 'machine' }["unqiue"]["value"],
-      total_counts_regular: dataset["total"]["buckets"].find {|access_method| access_method['key'] == 'regular' }["doc_count"],
-      total_counts_machine: dataset["total"]["buckets"].find {|access_method| access_method['key'] == 'machine' }["doc_count"]
+      unique_counts_regular: unique_regular.nil? ? 0 : unique_regular["unqiue"]["value"],
+      unique_counts_machine: unique_machine.nil? ? 0 : unique_machine["unqiue"]["value"],
+      total_counts_regular: total_regular.nil? ? 0 : total_regular["doc_count"],
+      total_counts_machine: total_machine.nil? ? 0:  total_machine["doc_count"]
     }
   end
 
   conn = Faraday.new(:url => API_URL)
-
+  logger = Logger.new(STDOUT)
+  
   arr = dois.map do |dataset| 
     puts dataset
     doi = dataset[:doi]
     json = conn.get "/works/#{doi}"
     next unless json.success?
+    logger.info "Success on getting metadata for #{doi}"
     data = JSON.parse(json.body)
     attributes = data["data"]["attributes"]
     { 
@@ -43,10 +52,11 @@ def filter(event)
       data_type: attributes["resource-type-id"],
       yop: attributes["published"],
       uri: attributes["identifier"],
-      publisher: attributes["publisher"],
+      publisher: attributes["container-title"],
       dataset_title: attributes["title"],
       publisher_id: attributes["data-center-id"],
       dataset_contributors: attributes["author"],
+      tags:["_dc_meta"]
     }
   end
 
