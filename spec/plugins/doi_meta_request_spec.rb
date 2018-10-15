936f4366e4804a4f570c629e2f8f511bc37c61b7
Kristian Garza <kj.garza@gmail.com>
2018-10-15 12:46:55 -0400

    format accordin to the CoP

---
+++ spec/plugins/doi_meta_request_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+
+describe "Discovery filter" do
+  let(:response)  { parse_reponse fixture_path + "/reponses/discovery_response"}
+
+  context "grokking single message" do
+
+  end
+
+end
