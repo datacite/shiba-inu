061a19627b023edc9e50152a50edd42451d4a10f
shahriyar9839 <73662595+shahriyar9839@users.noreply.github.com>
2021-06-19 18:22:09 +0430

    Creat

---
+++ kishu/test/test_changelog_generator.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "../util/changelog"
+require "rubygems/commands/setup_command"
+
+class ChangelogTest < Test::Unit::TestCase
+  def setup
+    @changelog = Changelog.for_rubygems(Gem::VERSION)
+  end
+
+  def test_format_header
+    Time.stub :now, Time.new(2020, 1, 1) do
+      assert_match Gem::Commands::SetupCommand::HISTORY_HEADER, @changelog.send(:format_header)
+    end
+  end
+end
